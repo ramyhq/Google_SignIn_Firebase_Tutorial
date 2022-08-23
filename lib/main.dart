1ebb6516260d9a17dc0d9e7461e98a475e85eae7
Ramy Wahid <ramy.dmk@gmail.com>
2022-08-23 21:59:17 +0200

    Adding Firebase Crashlytics in 3 Steps

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:zoom_colne_with_jitsi/screens/home_screen.dart';
 import 'package:zoom_colne_with_jitsi/screens/login_screen.dart';
@@ -7,6 +8,7 @@ import 'package:zoom_colne_with_jitsi/screens/video_call_creen.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   runApp(const MyApp());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:zoom_colne_with_jitsi/screens/meeting_creen.dart';
 
